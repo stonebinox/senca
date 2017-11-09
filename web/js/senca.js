c04b3dcf3cd0d7746e1fa951c793426bffdf642a
stonebinox <anoop.santhanam@gmail.com>
2017-11-09 14:19:33 +0530

    Layout"

--- web/js/senca.js
+++ web/js/senca.js
@@ -1,1 +1,7 @@
 var app=angular.module("senca",[]);
+app.controller("editor",function($scope,$compile,$http){
+    $scope.editor=null;
+    $scope.loadEditor=function(){
+        $scope.editor=new Quill("#editor");
+    };
+});
