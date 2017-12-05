30af6c79b3740d039e9b1907bc2ac09ae4d4df3b
stonebinox <anoop.santhanam@gmail.com>
2017-12-06 00:00:13 +0530

    Optimizing extraction

--- classes/urlMaster.php
+++ classes/urlMaster.php
@@ -234,7 +234,7 @@ class urlMaster
                     $dom = new DOMDocument();
                     @$dom->loadHTML($output);
                     $json=@$this->element_to_obj($dom->documentElement);
-                    $this->findHeadings($json['children']);
+                    @$this->findHeadings($json['children']);
                     foreach($this->headings as $heading)
                     {
                         $response=$content->addContent($heading,11,$urlID);
