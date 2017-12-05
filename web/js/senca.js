aee1167c097e664819a915495783651a3ef34e02
stonebinox <anoop.santhanam@gmail.com>
2017-12-06 01:56:41 +0530

    To show full content

--- web/js/senca.js
+++ web/js/senca.js
@@ -80,7 +80,8 @@ app.controller("editor",function($scope,$compile,$http){
             var text='';
             for(var i=0;i<content.length;i++){
                 var contentRow=content[i];
-                var contentValue=contentRow.content_value;
+                var contentID=content.idcontent_master;
+                var contentValue=stripslashes(contentRow.content_value);
                 $("#parser").html(contentValue);
                 var parsed=$("#parser").text();
                 $("#parser").html('');
@@ -89,11 +90,27 @@ app.controller("editor",function($scope,$compile,$http){
                 }
                 var contentType=contentRow.content_type_master_idcontent_type_master;
                 var contentTypeName=contentType.content_type;
-                text+='<div class="panel panel-default"><div class="panel-heading">'+contentTypeName+' result</div><div class="panel-body">'+parsed+'</div><div class="panel-footer"><div class="text-right"><div class="btn-group"><button type="button" class="btn btn-default btn-sm">Read full text</button><button type="button" class="btn btn-info btn-sm">Include <span class="glyphicon glyphicon-chevron-right"></span></button></div></div></div></div>';
+                text+='<div class="panel panel-default"><div class="panel-heading">'+contentTypeName+' result</div><div class="panel-body">'+parsed+'</div><div class="panel-footer"><div class="text-right"><div class="btn-group"><button type="button" class="btn btn-default btn-sm" ng-click="showFullContent('+contentID+')">Read full text</button><button type="button" class="btn btn-info btn-sm">Include <span class="glyphicon glyphicon-chevron-right"></span></button></div></div></div></div>';
             }
             text+='<br><br>';
             $("#suggestions").append(text);
+            $compile("#suggestions")($scope);
         }
+    };
+    $scope.showFullContent=function(contentID){
+        var content=$scope.contentArray;
+        for(var i=0;i<content.length;i++){
+            var cont=content[i];
+            if(cont.idcontent_master==contentID){
+                break;
+            }
+        }
+        var contentData=content[i];
+        var contentValue=stripslashes(contentData.content_value);
+        $("#parser").html(contentValue);
+        var parsed=$("#parser").text();
+        $("#parser").html('');
+        messageBox("Full Content",contentValue);
     };
 });
 var appURL=angular.module("sencaurl",[]);
